71dad70b950f680c276d08fed519f27a080915e5
Anameskhi <ameskhi@unisens.ge>
2022-12-26 20:57:16 +0400

    added product add with category

--- src/app/pages/manager/pages/products/product-add-edit/product-add-edit.component.ts
+++ src/app/pages/manager/pages/products/product-add-edit/product-add-edit.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
-import { map, Observable, Subject, takeUntil } from 'rxjs';
+import { map, Observable, of, Subject, switchMap, takeUntil } from 'rxjs';
 import { Category } from 'src/app/core/interfaces';
 import { ProductsService } from 'src/app/core/services';
 import { CategoryService } from 'src/app/core/services/category.service';
@@ -31,7 +31,7 @@ export class ProductAddEditComponent implements OnInit {
     id: new FormControl(null),
     name: new FormControl('',Validators.required),
     price: new FormControl('',Validators.required),
-    categoryId: new FormControl(''),
+    categoryId: new FormControl('',Validators.required),
     description: new FormControl('',Validators.required),
     image: new FormControl('',Validators.required)
   })
@@ -48,14 +48,26 @@ export class ProductAddEditComponent implements OnInit {
   productId: string | undefined
 
   ngOnInit(): void {
-    this.route.params
-    .pipe(takeUntil(this.sub$))
-    .subscribe(params => {
-      if(params['id']){
-        this.productId = params['id']
-        this.productsService.getOne(params['id']).subscribe(product => {
-          this.form.patchValue(product)
-        })
+    // this.route.params
+    // .pipe(takeUntil(this.sub$))
+    // .subscribe(params => {
+    //   if(params['id']){
+    //     this.productId = params['id']
+    //     this.productsService.getOne(params['id']).subscribe(product => {
+    //       this.form.patchValue(product)
+    //     })
+    //   }
+    // })
+    this.route.params.pipe(
+      switchMap((params: any)=>{
+        if(params['id']){
+          return this.productsService.getOne(params['id'])
+        }
+        return of(null)
+      })
+    ).subscribe(res => {
+      if(res){
+        this.form.patchValue(res)
       }
     })
   }
